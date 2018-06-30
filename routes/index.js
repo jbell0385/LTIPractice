d9c631243791f0e5cfd7efa3dbbf072be77f8a07
Jeremy Bell <jbell0385@gmail.com>
2018-06-30 16:25:09 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var lti = require('ims-lti');
 
 var cKey = "bbdf109cbc65fd11658b2a9c9d2fab9b";
 var cSecret = "1923358240917d4f82e7d0d2c3522195";
-
+var nonceStore = new lti.Stores.MemoryStore();
+console.log("nonceStore: ",nonceStore);
 
 
 /* GET home page. */
@@ -14,7 +15,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req,res)=>{
-  const provider = new lti.Provider(cKey, cSecret);
+  const provider = new lti.Provider(cKey, cSecret, nonceStore);
   provider.valid_request(req, (err, isValid)=>{
     if(err){console.log(err)};
     res.send(isValid);
