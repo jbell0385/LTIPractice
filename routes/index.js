3974ec3832f8f76554b1d7489321b32d9eca8867
Jeremy Bell <jbell0385@gmail.com>
2018-07-01 16:13:36 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var middleware = require('../middleware/middleware');
 var lti = require('ims-lti');
 
-var cKey = "jeremysecret";
-var cSecret = "jeremysecret";
+var cKey = "72397a6f8e18b80da92784ab3a1d981c";
+var cSecret = "6320fba80cd8c0b53fbc53c3a14fe619";
 // var nonceStore = new lti.Stores.MemoryStore();
 // console.log("nonceStore: ",nonceStore);
 
@@ -15,7 +15,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req,res)=>{
-  res.send("hello")
+  console.log("req.body----: ",req.body);
+  var provider = new lti.Provider(req.body, cSecret);
+  console.log("provider---: ",provider);
+  provider.valid_request(req, function(err, isValid) {
+    if(err){
+        console.log("LTI Error", err, isValid)
+    }
+    res.send(isValid);
+  });
 })
 
 module.exports = router;
