551f6de5e3fd64ba8b8b69a3ab171dd6784624f5
Jeremy Bell <jbell0385@gmail.com>
2018-07-02 11:43:30 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', (req,res)=>{
-  var cKey = "9da5e474af4430ccbcf1e948e244717d";
-  var cSecret = "308e962f3eec52178395b48322a62d4a";
+  var cKey = "96619f9a881aba6bda3d70f3e0eef6b3";
+  var cSecret = "108bcbd1b66458d36ce77dccd04bf601";
   var provider = new lti.Provider(cKey, cSecret);
-  provider.valid_request(req, function(err, isValid) {
-    if(err){
-        console.log("LTI Error", err, isValid)
-    }
-    console.log(req.body);
-    res.send(isValid);
-  });
+  console.log(provider);
+  // provider.valid_request(req, function(err, isValid) {
+  //   if(err){
+  //       console.log("LTI Error", err, isValid)
+  //   }
+  //   console.log(req.body);
+  //   res.send(isValid);
+  // });
 
 })
 
