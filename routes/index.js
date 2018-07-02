9a3df7c67289f7d1b1a4ef2975125ac74104e366
Jeremy Bell <jbell0385@gmail.com>
2018-07-02 08:40:58 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,13 @@ router.get('/', function(req, res, next) {
 router.post('/', (req,res)=>{
     var cKey = "d9c32887678fed93aaf76c0376a30988";
     var cSecret = "1454e9815d3b46e785b064c3202e6baf";
-    var ltiBody = JSON.parse(req.body);
+    var ltiBody = req.body;
     var provider = new lti.Provider(cKey, cSecret);;
     provider.valid_request(req, ltiBody, function(err, isValid) {
       if(err){
           console.log("LTI Error", err, isValid)
       }
-      res.send(isValid);
+      res.send(ltiBody);
     });
 })
 
