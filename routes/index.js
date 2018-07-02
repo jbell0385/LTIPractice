e1bb7cc708daa3229a90db309dff08f711f8e8e4
Jeremy Bell <jbell0385@gmail.com>
2018-07-02 11:51:19 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,8 @@ router.get('/', function(req, res, next) {
 router.post('/', (req,res)=>{
   var cKey = "96619f9a881aba6bda3d70f3e0eef6b3";
   var cSecret = "108bcbd1b66458d36ce77dccd04bf601";
-  var provider = new lti.Provider(cKey, cSecret);
+  var nonce_store = "MemoryStore";
+  var provider = new lti.Provider(cKey, cSecret, nonce_store);
   console.log(provider);
   // provider.valid_request(req, function(err, isValid) {
   //   if(err){
