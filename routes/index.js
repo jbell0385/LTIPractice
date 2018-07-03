4115ab89d5e9584f6d269240cd38717d096f8f83
Jeremy Bell <jbell0385@gmail.com>
2018-07-02 19:06:05 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.post('/', (req,res)=>{
   // console.log(req.body);
   var val = req.body;
   // console.log(val);
-  var provider = new lti.Provider(cKey, cSecret);
+  var provider = new lti.Provider(val, cSecret);
   //console.log(provider);
   provider.valid_request(req, function(err, isValid) {
     console.log("VAL: ",val);
